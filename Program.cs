410114f0a14cd0c3cc970f51b4bb03f07642bf5d
jeferson <jefersonfbh2010@gmail.com>
2026-08-12 21:03:02 -0300

    1172

--- Program.cs
+++ Program.cs
@@ -1,32 +1,97 @@
-using System; 
+int [] x = new int[10];
 
-
-class URI {
-
-    static void Main(string[] args) {
-int opcao = 1;
-while (opcao ==1 ) {
-
-int nota1 =int.Parse(Console.ReadLine()!);
-int nota2 =int.Parse(Console.ReadLine()!);
-
-if (nota1 < 0 || nota1 > 10 || nota2 < 0 || nota2 > 10) 
+for (int i = 0; i < 10; i++)
 {
-    Console.WriteLine("Nota inválida");
+    x[i] = int.Parse(Console.ReadLine());
+}
+
+if (x[0]<=0 )
+
+{
+
+    x[0] = 1;
     
-       
-}
-else if (nota1 >= 0 && nota1 <= 10 && nota2 >= 0 && nota2 <= 10)
-{
-    double media = (nota1 + nota2) / 2.0;
-    Console.WriteLine($"Média: {media:F2}");
     }
-Console.WriteLine ("Novo calculo (1-sim 2-nao)");
- opcao = int.Parse(Console.ReadLine());
 
-while (opcao != 1 && opcao != 2) {
-    opcao = int.Parse (Console.ReadLine());
-}
-}
-}  
-}
+     if (x[1] <= 0)
+{
+
+    x[1] = 1;
+    
+    }
+
+     if (x[2] <= 0)
+{
+
+    x[2] = 1;
+    
+    }
+
+ if (x[3] <= 0)
+{
+
+    x[3] = 1;
+    
+    }
+
+     if (x[4] <= 0)
+{
+
+    x[4] = 1;
+    
+    }
+
+     if (x[5] <= 0)
+{
+
+    x[5] = 1;
+    
+    }
+
+     if (x[6] <= 0)
+{
+
+    x[6] = 1;
+    
+    }
+
+     if (x[7] <= 0)
+{
+
+    x[7] = 1;
+    
+    }
+
+     if (x[8] <= 0)
+{
+
+    x[8] = 1;
+    
+    }
+
+     if (x[9] <= 0)
+{
+
+    x[9] = 1;
+    
+    }
+
+Console.WriteLine ($"X[0] = {x[0]}");
+
+Console.WriteLine ($"X[1] = {x[1]}");
+
+Console.WriteLine ($"X[2] = {x[2]}");
+
+Console.WriteLine ($"X[3] = {x[3]}");
+
+Console.WriteLine ($"X[4] = {x[4]}");
+
+Console.WriteLine ($"X[5] = {x[5]}");
+
+Console.WriteLine ($"X[6] = {x[6]}");
+
+Console.WriteLine ($"X[7] = {x[7]}");
+
+Console.WriteLine ($"X[8] = {x[8]}");
+
+Console.WriteLine ($"X[9] = {x[9]}");
